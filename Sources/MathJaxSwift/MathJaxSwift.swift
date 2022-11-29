160fff2b2f12f8b3187f12b72cae54d0d0cd49ba
Colin Campbell <colinc86@gmail.com>
2022-11-29 00:25:04 -0600

    Commit to save work.

--- Sources/MathJaxSwift/MathJaxSwift.swift
+++ Sources/MathJaxSwift/MathJaxSwift.swift
@@ -1,7 +1,24 @@
+//
+//  MathJaxSwift.swift
+//  MathJaxSwift
+//
+//  Created by Colin Campbell on 11/26/22.
+//
+
 import Foundation
 import JavaScriptCore
 
-/// A wrapper for the MathJax repo.
+/// An object that receives messages from a MathJax instance.
+public protocol MathJaxDelegate: AnyObject {
+
+  /// The MathJax instance received an exception.
+  ///
+  /// - Parameter exception: The exception that was received.
+  func mathJax(_ mathJax: MathJax, receivedException exception: String?)
+}
+
+/// A class that exposes the `tex2svg`, `tex2chtml`, and `tex2mml` MathJax 
+/// methods.
 public class MathJax {
   
   // MARK: Types
@@ -11,18 +28,31 @@ public class MathJax {
     
     /// The npm package file was missing.
     case missingPackageFile
+
+    /// The package-lock file's dependency information is missing for the 
+    // mathjax-full module.
+    case missingDependencyInformation
     
+    /// Unable to create the JS context.
     case missingJSContext
     
+    /// The package is missing the bundled JS file.
     case missingBundle
     
+    /// The conversion function could not be found in the bundled JS class.
     case missingFunction
     
+    /// The MathJax instance was deallocated.
     case deallocatedSelf
     
+    /// The instance failed to convert the input string.
     case conversionFailed
     
+    /// An unknown error occurred during conversion.
     case conversionUnknownError
+
+    /// The returned format was invalid.
+    case conversionInvalidFormat
   }
   
   // MARK: Private static properties
@@ -30,24 +60,34 @@ public class MathJax {
   /// The URL of the mjn top-level directory.
   private static let mjn = Bundle.module.url(forResource: "mjn", withExtension: nil)
   
+  /// The URL of the JS bundle file.
   private static let bundle = mjn?.appending(components: "dist", "mjn.bundle.js")
   
-  // MARK: Public static properties
-  
-  public static let base = mjn?.appending(components: "node_modules", "mathjax-full")
+  /// The URL of the mjn package-lock.json file.
+  private static let packageLock = mjn?.appending(path: "package-lock.json")
   
   // MARK: Private properties
   
+  /// The JS virtual machine.
   private let vm = JSVirtualMachine()
-  private let context: JSContext
-  private var tex2svgFunction: JSValue!
-  private var conversionQueue = DispatchQueue(label: "mathjax.swift.conversionQueue")
   
+  /// The JS context.
+  private let context: JSContext
+  
+  /// The TeX to SVG function.
+  private var tex2svgFunction: JSValue!
+
   // MARK: Public properties
+
+  /// The delegate to receive exceptions.
+  public weak var delegate: MathJaxDelegate?
   
   // MARK: Initializers
   
-  init() throws {
+  /// Initializes a new `MathJax` instance.
+  public init(delegate: MathJaxDelegate? = nil) throws {
+    self.delegate = delegate
+
     // Create the JavaScript context
     guard let ctx = JSContext(virtualMachine: vm) else {
       throw MathJaxError.missingJSContext
@@ -83,13 +123,17 @@ public class MathJax {
 extension MathJax {
   
   /// The MathJax npm package metadata.
-  public static func package() throws -> NPMPackage {
-    guard let packageURL = base?.appending(path: "package.json") else {
+  public static func package() throws -> NPMPackage.Dependency {
+    guard let packageLockURL = packageLock else {
       throw MathJaxError.missingPackageFile
     }
-    return try JSONDecoder().decode(
+    let package = try JSONDecoder().decode(
       NPMPackage.self,
       from: try Data(contentsOf: packageURL))
+    guard let dependency = package.dependencies.first(where: { $0.name == "mathjax-full" }) else {
+      throw MathJaxError.missingDependencyInformation
+    }
+    return dependency
   }
   
 }
@@ -98,6 +142,20 @@ extension MathJax {
 
 extension MathJax {
   
+  /// Converts a TeX input string to SVG.
+  ///
+  /// - Parameters:
+  ///   - input: The input string containing TeX.
+  ///   - inline: Process the math as inline or not.
+  ///   - em: The em-size in pixels.
+  ///   - ex: The ex-size in pixels.
+  ///   - width: Width of the container in pixels.
+  ///   - css: Output the CSS instead of the SVG.
+  ///   - styles: Include CSS styles for the image.
+  ///   - container: Include the `<mjx-container>` element.
+  ///   - fontCache: Wether or not a local font cache should be used.
+  ///   - assistiveMml: Whether to include assistive MathML output.
+  /// - Returns: Contents of an SVG.
   public func tex2svg(
     _ input: String,
     inline: Bool = false,
@@ -118,7 +176,7 @@ extension MathJax {
       
       Task {
         do {
-          if let output = try self.tex2svg(
+          continuation.resume(returning: try self.tex2svg(
             input,
             inline: inline,
             em: em,
@@ -129,12 +187,7 @@ extension MathJax {
             container: container,
             fontCache: fontCache,
             assistiveMml: assistiveMml
-          ) {
-            continuation.resume(returning: output)
-          }
-          else {
-            continuation.resume(throwing: MathJaxError.conversionUnknownError)
-          }
+          ))
         }
         catch {
           continuation.resume(throwing: error)
@@ -143,50 +196,20 @@ extension MathJax {
     }
   }
   
-//  private func tex2svg(
-//    input: String,
-//    inline: Bool = false,
-//    em: Float = 16,
-//    ex: Float = 8,
-//    width: Float = 80 * 16,
-//    css: Bool = false,
-//    styles: Bool = true,
-//    container: Bool = false,
-//    fontCache: Bool = true,
-//    assistiveMml: Bool = false,
-//    completion: @escaping (String?, Error?) -> Void
-//  ) {
-//    conversionQueue.async { [weak self] in
-//      guard let self = self else {
-//        completion(nil, MathJaxError.deallocatedSelf)
-//        return
-//      }
-//
-//      guard let value = self.tex2svgFunction?.call(withArguments: [
-//        input,
-//        inline,
-//        em,
-//        ex,
-//        width,
-//        css,
-//        styles,
-//        container,
-//        fontCache,
-//        assistiveMml
-//      ]) else {
-//        completion(nil, MathJaxError.conversionFailed)
-//        return
-//      }
-//
-//      if let stringValue = value.toString() {
-//        completion(stringValue, nil)
-//      }
-//      else {
-//        completion(nil, MathJaxError.conversionUnknownError)
-//      }
-//    }
-//  }
-  
+  /// Converts a TeX input string to SVG.
+  ///
+  /// - Parameters:
+  ///   - input: The input string containing TeX.
+  ///   - inline: Process the math as inline or not.
+  ///   - em: The em-size in pixels.
+  ///   - ex: The ex-size in pixels.
+  ///   - width: Width of the container in pixels.
+  ///   - css: Output the CSS instead of the SVG.
+  ///   - styles: Include CSS styles for the image.
+  ///   - container: Include the `<mjx-container>` element.
+  ///   - fontCache: Wether or not a local font cache should be used.
+  ///   - assistiveMml: Whether to include assistive MathML output.
+  /// - Returns: Contents of an SVG.
   public func tex2svg(
     _ input: String,
     inline: Bool = false,
@@ -198,8 +221,8 @@ extension MathJax {
     container: Bool = false,
     fontCache: Bool = true,
     assistiveMml: Bool = false
-  ) throws -> String? {
-    guard let value = self.tex2svgFunction?.call(withArguments: [
+  ) throws -> String {
+    return try callFunction(tex2svgFunction, with: [
       input,
       inline,
       em,
@@ -210,16 +233,7 @@ extension MathJax {
       container,
       fontCache,
       assistiveMml
-    ]) else {
-      throw MathJaxError.conversionFailed
-    }
-    
-    if let stringValue = value.toString() {
-      return stringValue
-    }
-    else {
-      throw MathJaxError.conversionUnknownError
-    }
+    ])
   }
   
 }
@@ -228,16 +242,29 @@ extension MathJax {
 
 extension MathJax {
   
+  /// Handles an exception from the JS context.
+  ///
+  /// - Parameters:
+  ///   - context: The JS context that produced the exception.
+  ///   - value: The value passed as an exception.
   private func handleException(from context: JSContext?, value: JSValue?) {
-    guard let value = value else {
-      print("Received exception: (no value)")
-      return
+    delegate?.mathJax(self, receivedException: value?.toString())
+  }
+
+  /// Calls the function with the given arguments.
+  ///
+  /// - Parameters:
+  ///   - function: The function to call.
+  ///   - arguments: The arguments to pass to the function.
+  /// - Returns: The function's return value.
+  private func callFunction(_ function: JSValue?, with arguments: [Any]) throws -> String {
+    guard let value = function?.call(withArguments: arguments) else {
+      throw MathJaxError.conversionFailed
     }
     guard let stringValue = value.toString() else {
-      print("Received exception: (unable to get string value)")
-      return
+      throw MathJaxError.conversionInvalidFormat
     }
-    print("Received exception: \(stringValue)")
+    return stringValue
   }
   
 }
--- Sources/MathJaxSwift/NPMPackage.swift
+++ Sources/MathJaxSwift/NPMPackage.swift
@@ -1,28 +1,22 @@
 //
 //  NPMPackage.swift
-//  
+//  MathJaxSwift
 //
 //  Created by Colin Campbell on 11/26/22.
 //
 
 import Foundation
 
-/// NPM package metadata.
-///
-/// For more information see https://docs.npmjs.com/cli/v9/configuring-npm/package-json
-public struct NPMPackage: Codable {
+/// NPM package-lock.json metadata for extracting the mathjax-full version 
+/// string.
+internal struct NPMPackage: Codable {
+
+  struct Dependency: Codable {
+    let version: String
+    let resolved: URL
+    let integrity: String
+  }
   
-  public let name: String?
-  public let version: String?
-  public let description: String?
-  public let keywords: [String]?
-  public let devDependencies: [String: String]?
-  public let maintainers: [String]?
-  public let bugs: [String: String]?
-  public let license: String?
-  public let repository: [String: String]?
-  public let main: String?
-  public let files: [String]?
-  public let scripts: [String: String]?
+  let dependencies: [String: Metadata]
   
 }
